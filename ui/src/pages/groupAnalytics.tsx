cb6af5fe3becb6ded2376a1b21c3e152a636015b
James Acklin <james.acklin@gmail.com>
2023-04-11 17:02:05 -0400

    group: produce list of all authors

--- ui/src/pages/groupAnalytics.tsx
+++ ui/src/pages/groupAnalytics.tsx
@@ -1,91 +1,13 @@
 import React, { useEffect, useState } from "react";
+import cn from "classnames";
 import _ from "lodash";
 import { useParams } from "react-router-dom";
 import {
-  useQuery,
-  useQueries,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from "react-query";
-import { useUrbit } from "../logic/useUrbit";
-
-function useChannels() {
-  const api = useUrbit();
-  const { ship, group } = useParams();
-  return useQuery(
-    ["channels", ship, group],
-    async () => {
-      const g = await api.scry({
-        app: "groups",
-        path: `/groups/${ship}/${group}`,
-      });
-      return g.channels;
-    },
-    { enabled: !!ship && !!group }
-  );
-}
-
-function useWrits(channels: any) {
-  const api = useUrbit();
-  const writs = useQueries(
-    _.map(channels, (channel: any) => {
-      return {
-        queryKey: ["writs", channel],
-        queryFn: async () => {
-          const c = await api.scry({
-            app: "chat",
-            path: `/${channel}/writs/newest/999.999.999`,
-          });
-          return c;
-        },
-        enabled: !!channel && !!api,
-      };
-    })
-  );
-  return writs;
-}
-
-function useCurios(channels: any) {
-  const api = useUrbit();
-  const curios = useQueries(
-    _.map(channels, (channel: any) => {
-      return {
-        queryKey: ["curios", channel],
-        queryFn: async () => {
-          const c = await api.scry({
-            app: "heap",
-            path: `/${channel}/curios/newest/999.999.999`,
-          });
-          return c;
-        },
-        enabled: !!channel && !!api,
-      };
-    })
-  );
-  return curios;
-}
-
-function useNotes(channels: any) {
-  const api = useUrbit();
-  const notes = useQueries(
-    _.map(channels, (channel: any) => {
-      return {
-        queryKey: ["notes", channel],
-        queryFn: async () => {
-          const c = await api.scry({
-            app: "diary",
-            path: `/${channel}/notes/newest/999.999.999/outline`,
-          });
-          return c;
-        },
-        enabled: !!channel && !!api,
-      };
-    })
-  );
-  return notes;
-}
+  useChannels,
+  useWrits,
+  useCurios,
+  useNotes,
+} from "../logic/useContent";
 
 export function GroupAnalytics() {
   const { ship, group } = useParams();
@@ -97,10 +19,30 @@ export function GroupAnalytics() {
     )
   );
 
+  const memos = _.flatten(
+    _.map(writs, (writ: any) => {
+      return _.map(writ.data, (writ: any) => {
+        return {
+          ...writ.memo,
+        };
+      });
+    })
+  );
+
   const curios = useCurios(
     _.keys(groupChannels?.data).filter((channel: any) =>
       channel.includes("heap")
     )
+  );
+
+  const hearts = _.flatten(
+    _.map(curios, (curio: any) => {
+      return _.map(curio.data, (curio: any) => {
+        return {
+          ...curio.heart,
+        };
+      });
+    })
   );
 
   const notes = useNotes(
@@ -109,7 +51,22 @@ export function GroupAnalytics() {
     )
   );
 
-  console.log(notes);
+  const outlines = _.flatten(
+    _.map(notes, (note: any) => {
+      return _.map(note.data, (note: any) => {
+        return {
+          ...note,
+        };
+      });
+    })
+  );
+
+  const isAnyPending =
+    _.some(curios, { status: "loading" }) ||
+    _.some(notes, { status: "loading" });
+
+  const allContent = _.concat(memos, hearts, outlines);
+  const allAuthors = _.map(_.uniqBy(allContent, "author"), "author");
 
   return (
     <div className="mx-auto max-w-xl font-mono">
@@ -126,6 +83,11 @@ export function GroupAnalytics() {
       </div>
       <div>
         {_.some(notes, { status: "loading" }) ? "Loading notes..." : ""}
+      </div>
+      <div className={cn({ "text-gray-300": isAnyPending })}>
+        {_.map(allAuthors, (author) => (
+          <div>{author}</div>
+        ))}
       </div>
     </div>
   );
