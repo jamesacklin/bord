aa768524bc33d29ca9b209834157864856e53228
James Acklin <james.acklin@gmail.com>
2023-04-11 19:45:23 -0400

    groups: show author counts

--- ui/src/pages/groupAnalytics.tsx
+++ ui/src/pages/groupAnalytics.tsx
@@ -8,6 +8,7 @@ import {
   useCurios,
   useNotes,
 } from "../logic/useContent";
+import { Spinner } from "../components/spinner";
 
 export function GroupAnalytics() {
   const { ship, group } = useParams();
@@ -67,28 +68,51 @@ export function GroupAnalytics() {
 
   const allContent = _.concat(memos, hearts, outlines);
   const allAuthors = _.map(_.uniqBy(allContent, "author"), "author");
+  const authorCounts = _.countBy(allContent, "author");
+  const sortedAuthors = _.chain(allAuthors)
+    .map((author) => {
+      return { author, count: authorCounts[author] };
+    })
+    .orderBy("count", "desc")
+    .value();
 
   return (
-    <div className="mx-auto max-w-xl font-mono">
-      <h1>
-        {" "}
+    <div className="mx-auto max-w-2xl font-mono overflow-scroll">
+      <h1 className="my-4">
         {ship}/{group}
       </h1>
-      <div>{groupChannels.isFetching ? "Loading channel list..." : null}</div>
-      <div>
-        {_.some(writs, { status: "loading" }) ? "Loading writs..." : ""}
-      </div>
-      <div>
-        {_.some(curios, { status: "loading" }) ? "Loading curios..." : ""}
-      </div>
-      <div>
-        {_.some(notes, { status: "loading" }) ? "Loading notes..." : ""}
-      </div>
-      <div className="mt-4">{allAuthors.length + 1} unique authors</div>
-      <div className={cn({ "text-gray-300": isAnyPending })}>
-        {_.map(allAuthors, (author) => (
-          <div>{author}</div>
-        ))}
+
+      <div
+        className={cn(
+          "relative pb-4",
+          isAnyPending ? "text-gray-400" : "text-black"
+        )}
+      >
+        {isAnyPending && (
+          <div className="absolute top-32 inset-x-2/4 -translate-x-4 -translate-y-4 ">
+            <Spinner />
+          </div>
+        )}
+
+        <div>{allAuthors.length + 1} unique authors</div>
+        <table className="w-full">
+          <thead>
+            <tr>
+              <th className="text-left">Author</th>
+              <th className="text-right">Count</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sortedAuthors.map((author) => {
+              return (
+                <tr className="even:bg-gray-100">
+                  <td className="text-left w-10/12 pl-2">{author.author}</td>
+                  <td className="text-right w-2/12 pr-2">{author.count}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     </div>
   );
