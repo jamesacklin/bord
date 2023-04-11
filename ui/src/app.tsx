40e98b8185e55a6e19727a4c07d6a45cf73c193b
James Acklin <james.acklin@gmail.com>
2023-04-11 13:54:54 -0400

    group: use react-query for fetching group content

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -1,17 +1,28 @@
 import React from "react";
 import _ from "lodash";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { Members } from "./pages/members";
 import { GroupAnalytics } from "./pages/groupAnalytics";
 
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 export function App() {
   return (
-    <Router basename="/apps/bord">
-      <Routes>
-        <Route index element={<div>hi</div>} />
-        <Route path="/:ship/:group" element={<GroupAnalytics />} />
-        <Route path="/members" element={<Members />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router basename="/apps/bord">
+        <Routes>
+          <Route index element={<div>hi</div>} />
+          <Route path="/:ship/:group" element={<GroupAnalytics />} />
+          <Route path="/members" element={<Members />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 }
--- ui/src/pages/groupAnalytics.tsx
+++ ui/src/pages/groupAnalytics.tsx
@@ -1,47 +1,132 @@
 import React, { useEffect, useState } from "react";
 import _ from "lodash";
 import { useParams } from "react-router-dom";
+import {
+  useQuery,
+  useQueries,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "react-query";
 import { useUrbit } from "../logic/useUrbit";
 
-export function GroupAnalytics() {
+function useChannels() {
   const api = useUrbit();
   const { ship, group } = useParams();
-  const [channels, setChannels] = useState<any>([]);
-
-  useEffect(() => {
-    async function getChannels() {
+  return useQuery(
+    ["channels", ship, group],
+    async () => {
       const g = await api.scry({
         app: "groups",
         path: `/groups/${ship}/${group}`,
       });
-      setChannels(g.channels);
-    }
+      return g.channels;
+    },
+    { enabled: !!ship && !!group }
+  );
+}
 
-    getChannels();
-  }, []);
+function useWrits(channels: any) {
+  const api = useUrbit();
+  const writs = useQueries(
+    _.map(channels, (channel: any) => {
+      return {
+        queryKey: ["writs", channel],
+        queryFn: async () => {
+          const c = await api.scry({
+            app: "chat",
+            path: `/${channel}/writs/newest/999.999.999`,
+          });
+          return c;
+        },
+        enabled: !!channel && !!api,
+      };
+    })
+  );
+  return writs;
+}
 
-  useEffect(() => {
-    if (channels.length === 0) return;
-    async function getChatData() {
-      const chatChannels = _.filter(_.keys(channels), (channel) => {
-        return channel.startsWith("chat/");
-      });
-      const data = await api.scry({
-        app: "chat",
-        path: `/${chatChannels[6]}/writs/newest/999.999.999`,
-      });
-      console.log(data);
-    }
-    getChatData();
-  }, [channels]);
+function useCurios(channels: any) {
+  const api = useUrbit();
+  const curios = useQueries(
+    _.map(channels, (channel: any) => {
+      return {
+        queryKey: ["curios", channel],
+        queryFn: async () => {
+          const c = await api.scry({
+            app: "heap",
+            path: `/${channel}/curios/newest/999.999.999`,
+          });
+          return c;
+        },
+        enabled: !!channel && !!api,
+      };
+    })
+  );
+  return curios;
+}
 
-  console.log(channels);
+function useNotes(channels: any) {
+  const api = useUrbit();
+  const notes = useQueries(
+    _.map(channels, (channel: any) => {
+      return {
+        queryKey: ["notes", channel],
+        queryFn: async () => {
+          const c = await api.scry({
+            app: "diary",
+            path: `/${channel}/notes/newest/999.999.999/outline`,
+          });
+          return c;
+        },
+        enabled: !!channel && !!api,
+      };
+    })
+  );
+  return notes;
+}
+
+export function GroupAnalytics() {
+  const { ship, group } = useParams();
+  const groupChannels = useChannels();
+
+  const writs = useWrits(
+    _.keys(groupChannels?.data).filter((channel: any) =>
+      channel.includes("chat")
+    )
+  );
+
+  const curios = useCurios(
+    _.keys(groupChannels?.data).filter((channel: any) =>
+      channel.includes("heap")
+    )
+  );
+
+  const notes = useNotes(
+    _.keys(groupChannels?.data).filter((channel: any) =>
+      channel.includes("diary")
+    )
+  );
+
+  console.log(notes);
 
   return (
-    <div>
-      <h1>Group Analytics</h1>
-      <p>Ship: {ship}</p>
-      <p>Group: {group}</p>
+    <div className="mx-auto max-w-xl font-mono">
+      <h1>
+        {" "}
+        {ship}/{group}
+      </h1>
+      <div>{groupChannels.isFetching ? "Loading channel list..." : null}</div>
+      <div>
+        {_.some(writs, { status: "loading" }) ? "Loading writs..." : ""}
+      </div>
+      <div>
+        {_.some(curios, { status: "loading" }) ? "Loading curios..." : ""}
+      </div>
+      <div>
+        {_.some(notes, { status: "loading" }) ? "Loading notes..." : ""}
+      </div>
     </div>
   );
 }
